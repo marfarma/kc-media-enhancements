78c19fc0f45500a50bc5fe7b7109a20fb2d09944
kucrut <kucrut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-11 20:16:01 +0000

    Version 0.3
    git-svn-id: http://svn.wp-plugins.org/kc-media-enhancements/tags/0.3@584359 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- kc-media-enhancements.php
+++ kc-media-enhancements.php
@@ -4,132 +4,119 @@
 Plugin name: KC Media Enhancements
 Plugin URI: http://kucrut.org/2011/04/kc-media-enhancements/
 Description: Enhance WordPress media/attachment management
-Version: 0.1
+Version: 0.3
 Author: Dzikri Aziz
 Author URI: http://kucrut.org/
 License: GPL v2
+Text Domain: kc-media-enhancements
 */
 
 class kcMediaEnhancements {
-	function __construct() {
-		$this->prefix = 'kc-media-enhancements';
-		$this->inc_path = dirname(__FILE__) . '/kc-media-enhancements-inc';
+	private static $data = array(
+		'defaults' => array(
+			'general' => array(
+				'components' => array( 'insert_custom_size', 'taxonomies' ),
+				'taxonomies' => array( 'category', 'post_tag' )
+			)
+		)
+	);
+
+
+	public static function prepare() {
+		add_image_size( 'kcme-test', 1000, 200, true );
+
+		self::$data['inc_path'] = dirname(__FILE__) . '/kc-media-enhancements-inc';
+		self::$data['kcSettingsOK'] = class_exists( 'kcSettings' );
 
 		# i18n
-		load_plugin_textdomain( $this->prefix, false, $this->prefix . '/kc-media-enhancements-inc/languages' );
+		load_plugin_textdomain( 'kc-media-enhancements', false, 'kc-media-enhancements/kc-media-enhancements-inc/languages' );
 
-		# Load helpers
-		require_once( $this->inc_path . '/helpers.php' );
-
-		add_filter( 'kc_plugin_settings', array(&$this, 'settings') );
-		add_action( 'init', array(&$this, 'init'), 13 );
+		add_filter( 'kc_plugin_settings', array(__CLASS__, 'settings') );
+		add_action( 'admin_init', array(__CLASS__, 'init') );
 	}
 
 
-	function init() {
-		$this->options();
-		$this->magick();
-
-		//add_action( 'admin_footer', array(&$this, 'dev') );
-	}
-
-
-	function components() {
-		$components = array(
-			'insert_custom_size'	=> __('Enable insertion of images with custom sizes into post editor', $this->prefix),
-			'taxonomies'					=> __('Enable taxonomies for attachments', $this->prefix)
-		);
-
-		return $components;
-	}
-
-
-	function options() {
-		if ( function_exists('kc_get_option') ) {
-			$this->options = kc_get_option( $this->prefix );
-		}
-		else {
-			$options = array(
-				'general' => array(
-					'components'	=> array_map('__return_true', $this->components() ),
-					'taxonomies'	=> array_map('__return_true', kc_get_public_taxonomies() )
-				)
-			);
-
-			$this->options = apply_filters( 'kcme_options', $options );
-		}
-	}
-
-
-	function settings( $groups ) {
-		$my_group = array(
-			'prefix'				=> $this->prefix,
-			'menu_title'		=> __('KC Media Enhancements', $this->prefix),
-			'page_title'		=> __('KC Media Enhancements Settings', $this->prefix),
-			'options'				=> array(
-				'general'		=> array(
-					'id'			=> 'general',
-					'title'		=> __('General', $this->prefix),
-					'fields'	=> array(
+	public static function settings( $groups ) {
+		$groups[] = array(
+			'prefix'     => 'kc-media-enhancements',
+			'menu_title' => 'KC Media Enhc.',
+			'page_title' => sprintf( __('%s Settings', 'kc-media-enhancements'), 'KC Media Enhancements' ),
+			'options'    => array(
+				'general'  => array(
+					'id'     => 'general',
+					'title'  => __('General', 'kc-media-enhancements'),
+					'fields' => array(
 						array(
-							'id'			=> 'components',
-							'title'		=> __('Components', $this->prefix),
-							'type'		=> 'checkbox',
-							'options'	=> $this->components()
+							'id'      => 'components',
+							'title'   => __('Components', 'kc-media-enhancements'),
+							'type'    => 'checkbox',
+							'options' => array(
+								'insert_custom_size' => __('Enable insertion of images with custom sizes into post editor', 'kc-media-enhancements'),
+								'taxonomies'         => __('Enable taxonomies for attachments', 'kc-media-enhancements')
+							)
 						),
 						array(
-							'id'			=> 'taxonomies',
-							'title'		=> __('Taxonomies for attachments', $this->prefix),
-							'type'		=> 'checkbox',
-							'options'	=> kc_get_public_taxonomies()
+							'id'      => 'taxonomies',
+							'title'   => __('Taxonomies for attachments', 'kc-media-enhancements'),
+							'type'    => 'checkbox',
+							'options' => kcSettings_options::$taxonomies
 						)
 					)
 				)
 			)
 		);
 
-		$groups[] = $my_group;
 		return $groups;
 	}
 
 
-	function magick() {
-		$components = $this->options['general']['components'];
+	public static function init() {
+		if ( self::$data['kcSettingsOK'] )
+			$options = kc_get_option( 'kc-media-enhancements' );
+		else
+			$options = apply_filters( 'kcme_options', self::$data['defaults'] );
+
+		self::$data['options'] = $options;
+		if ( !isset($options['general']['components']) || !is_array($options['general']['components']) || empty($options['general']['components']) )
+			return;
 
 		# 0. Insert image with custom sizes
-		if ( isset($components['insert_custom_size']) && $components['insert_custom_size'] ) {
-			require_once( $this->inc_path . '/insert_custom_size.php' );
-			add_filter( 'attachment_fields_to_edit', 'kc_additional_image_size_input_fields', 11, 2 );
-		}
+		if ( in_array('insert_custom_size', $options['general']['components']) )
+			add_filter( 'image_size_names_choose', array(__CLASS__, 'insert_image'), 99 );
 
-		# 1. Attachment taxonomie
-		if ( isset($components['taxonomies'])
-					&& $components['taxonomies']
-					&& isset($this->options['general']['taxonomies'])
-					&& is_array($this->options['general']['taxonomies'])
-					&& !empty($this->options['general']['taxonomies']) ) {
+		# 1. Attachment taxonomies
+		if (
+			!in_array('taxonomies', $options['general']['components'])
+			|| !is_array($options['general']['taxonomies'])
+			|| empty($options['general']['taxonomies'])
+		)
+			return;
 
-			$taxonomies = array();
-			foreach ( $this->options['general']['taxonomies'] as $key => $val )
-				if ( $val )
-					$taxonomies[] = $key;
+		$taxonomies = array();
+		foreach ( $options['general']['taxonomies'] as $tax )
+			if ( taxonomy_exists( $tax ) )
+				$taxonomies[] = $tax;
 
-			if ( !empty($taxonomies) ) {
-				require_once( $this->inc_path . '/attachment_taxonomies.php' );
-				$do = new kcAttachmentTaxonomies( $taxonomies );
-			}
+		if ( !empty($taxonomies) ) {
+			require_once( self::$data['inc_path'] . '/attachment_taxonomies.php' );
+			kcmeAttachmentTaxonomies::init( $taxonomies );
 		}
 	}
 
 
-	function dev() {
-		echo '<pre>';
+	public static function insert_image( $sizes ) {
+		global $_wp_additional_image_sizes;
+		if ( empty($_wp_additional_image_sizes) )
+			return $sizes;
 
+		foreach ( $_wp_additional_image_sizes as $id => $data ) {
+			if ( !isset($sizes[$id]) )
+				$sizes[$id] = ucfirst( str_replace( '-', ' ', $id ) );
+		}
 
-		echo '</pre>';
+		return $sizes;
 	}
 }
-
-$kcMediaEnhancements = new kcMediaEnhancements;
+add_action( 'plugins_loaded', array('kcMediaEnhancements', 'prepare') );
 
 ?>
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: kucrut
 Donate link: http://kucrut.org/
 Tags: media, attachment, taxonomy, terms, category, tag
 Requires at least: 3.0
-Tested up to: 3.1
-Stable tag: 0.1
+Tested up to: 3.4.1
+Stable tag: 0.3
 
 Enhance WordPress media/attachment management
 
@@ -16,31 +16,21 @@ This plugin provides enhancements for media/attachment management, such as the a
 
 1. Use standard WordPress plugin installation or upload the `kc-media-enhancements` directory to your `wp-content/plugins` directory
 2. Activate the plugin through the 'Plugins' menu
-3. Read the FAQ section on readme.txt if you need to enable only certain components
+3. Read the FAQ
 
 == Frequently Asked Questions ==
 
 = Can I only activate components I need? =
-##### Short answer:
-Sure!
+Sure! By default, all components are enabled. If you have [KC Settings plugin] (http://wordpress.org/extend/plugins/kc-settings/) installed and activated, you'll have the luxury to select the components you need by visiting *Settings* &raquo; *KC Media Enhc.* in you dashboard.
 
-##### Long Answer:
-By default, all components are enabled. If you have [KC Settings plugin] (http://wordpress.org/extend/plugins/kc-settings/) installed and activated, you'll have the luxury to select the components you need by visiting *Settings* &raquo; *KC Media Enhancements* in you dashboard.
-
-If you don't want to use KC Setting plugin but still want to enable only certain components, you'll need to this block of code to your theme's `functions.php` file and change each unwanted component's value to `false`:
+If you don't want to use KC Setting plugin but still want to enable only certain components, you'll need to add this block of code to your theme's `functions.php` file and change each unwanted component's value to `false`:
 
 `
 function my_kcme_options( $options ) {
 	$options = array(
 		'general' => array(
-			'components' => array(
-				'insert_custom_size'	=> true,
-				'taxonomies'					=> true
-			),
-			'taxonomies'	=> array(
-				'category'	=> true,
-				'post_tag'	=> true
-			)
+			'components' => array( 'insert_custom_size', 'taxonomies' ),
+			'taxonomies' => array( 'category', 'post_tag' )
 		)
 	);
 
@@ -49,9 +39,13 @@ function my_kcme_options( $options ) {
 add_filter( 'kcme_options', 'my_kcme_options' );
 `
 
-Please replace the taxonomies array keys with the taxonomy names you want to set for the attachment post type.
+Please replace the taxonomies array with the taxonomy names you want to set for the attachment post type.
 
 Options saved by KC Settings will always get the highest priority when the plugin is active.
+
+= I Don't see the custom size option when I tried to insert an image into posts =
+Either the original image dimension is smaller than the custom size, or you added the custom size *after* the image has been uploaded.
+If this is the case, you need to rebuild the image's thumbnails using Viper007Bond's excellent plugin: [Regenerate Thumbnails] (http://wordpress.org/extend/plugins/regenerate-thumbnails/)
 
 == Screenshots ==
 1. Settings page
